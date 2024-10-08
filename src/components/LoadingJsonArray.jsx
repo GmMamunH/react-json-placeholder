fb1ce312718fd6118b3f42b489bd1973e7c561c3
Md Mamun Hossain <gmmamun3004@gmail.com>
2024-10-08 18:54:31 +0600

    LoadingJsonArray.jsx

---
+++ src/components/LoadingJsonArray.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react'
+
+const LoadingJsonArray = () => {
+  
+  return (
+    <div>LoadingJsonArray</div>
+  )
+}
+
+export default LoadingJsonArray
