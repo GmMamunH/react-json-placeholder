50a703caac3cc1743d3aee3940f9dd188760076a
Md Mamun Hossain <gmmamun3004@gmail.com>
2024-10-10 18:38:57 +0600

    data fetch

--- src/components/page/LandingPage.jsx
+++ src/components/page/LandingPage.jsx
@@ -1,8 +1,16 @@
 /* eslint-disable no-unused-vars */
-
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const LandingPage = () => {
+    const [users, setUsers]=useState([])
+    useEffect(()=>{
+        axios.get("https://jsonplaceholder.typicode.com/users").then((data)=>{
+            console.log(data);
+          
 
+        });
+    },[])
   return (
     <div>LandingPage</div>
   )
