7defc94b56e8ea85add72194ccdaa089dbbc9627
Md Mamun Hossain <gmmamun3004@gmail.com>
2024-10-10 21:26:36 +0600

    [Update] ui design update

--- src/components/page/LandingPage.jsx
+++ src/components/page/LandingPage.jsx
@@ -1,6 +1,10 @@
 /* eslint-disable no-unused-vars */
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { FaPhoneAlt } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+import { MdLocationCity } from "react-icons/md";
+import { RiGlobalFill } from "react-icons/ri";
 
 const LandingPage = () => {
   const [users, setUsers] = useState([]);
@@ -11,8 +15,8 @@ const LandingPage = () => {
   }, []);
   return (
     <>
-      <header className=" bg-white shadow-lg">
-        <nav className="flex justify-between items-center py-5 container mx-auto">
+      <header className=" bg-white shadow">
+        <nav className="flex justify-between items-center py-2 container mx-auto">
           <section>
             <h1 className="text-base text-black font-bold ">
               JSONPlaceholder{" "}
@@ -53,27 +57,59 @@ const LandingPage = () => {
       {/* ==================================================== */}
 
       <section>
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-5 mt-5 p-5">
+        <div className="grid grid-cols-1  gap-5 mt-5 max-w-screen-sm mx-auto">
           {users.map((user) => (
             <div key={user?.id}>
-              <div className="border shadow-lg rounded-lg  p-3">
-                <p>
-                  <strong>Name: </strong> {user?.name}
-                </p>
-                <p>
-                  <strong>Phone: </strong> {user?.phone}
-                </p>
-                <p>
-                  <strong>Address: </strong> {user?.address?.street},{" "}
-                  {user?.address?.suite}, {user?.address?.city}
-                </p>
-                
-                <p>
-                  <strong>Website: </strong> {user?.website}
-                </p>
-                <p>
-                  <strong>Company: </strong> {user?.company?.name}
-                </p>
+              <div className="border shadow rounded-md  p-8 ">
+                <div className="flex flex-col gap-4">
+                  <section>
+                    <p className="text-2xl font-bold hover:underline duration-300 underline-offset-8">
+                      {user?.name}
+                    </p>
+                  </section>
+
+                  <section>
+                    <p className="flex gap-2 items-center">
+                      <FaPhoneAlt />
+                      <span className="text-sm text-gray-900 font-normal">
+                        {user?.phone}
+                      </span>
+                    </p>
+                    <p className="flex gap-2 items-center">
+                      <FaLocationDot />
+                      <span className="text-sm text-gray-900 font-normal">
+                        {user?.address?.street}, {user?.address?.suite},{" "}
+                        {user?.address?.city}
+                      </span>
+                    </p>
+
+                    <p className="flex gap-2 items-center">
+                      {" "}
+                      <RiGlobalFill />
+                      <span className="text-sm text-gray-900 font-normal">
+                        {" "}
+                        {user?.website}{" "}
+                      </span>
+                    </p>
+                    <p className="flex gap-2 items-center">
+                      <MdLocationCity />
+                      <span className="text-sm text-gray-900 font-normal">
+                        {user?.company?.name}{" "}
+                      </span>
+                    </p>
+                  </section>
+                  <section className="flex gap-4">
+                    <button className="bg-black text-white rounded-md px-4 py-2 font-medium text-sm">
+                      Posts
+                    </button>
+                    <button className="bg-black text-white rounded-md px-4 py-2 font-medium text-sm">
+                      Albums
+                    </button>
+                    <button className="bg-black text-white rounded-md px-4 py-2 font-medium text-sm">
+                      Todos
+                    </button>
+                  </section>
+                </div>
               </div>
             </div>
           ))}
